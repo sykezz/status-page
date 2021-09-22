1b34260c4f9c11108b74cffc02395d4cd5cd5d7f
sykez <hello@sykez.com>
2021-09-22 16:24:51 +0800

    statusfy

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "sykez.com",
+  "name": "sykez.com",
+  "description": "",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
